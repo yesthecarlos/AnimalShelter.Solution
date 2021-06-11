2553784735e9e45b0f4245296cc5676ef6d92a67
yesthecarlos <carlosmendez86@gmail.com>
2021-06-11 15:38:37 -0700

    finish create and update functionality

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -23,13 +23,26 @@ namespace AnimalShelter.Controllers
       return await _db.Animals.ToListAsync();
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+
+      if (animal == null)
+      {
+          return NotFound();
+      }
+
+      return animal;
+    }
+
     [HttpPost]
     public async Task<ActionResult<Animal>> Post(Animal animal)
     {
       _db.Animals.Add(animal);
       await _db.SaveChangesAsync();
 
-      return CreatedAtAction("Post", new { id = animal.AnimalId }, animal);
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
     }
   }
 }
