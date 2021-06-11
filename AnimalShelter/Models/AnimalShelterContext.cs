98f6cb98c8cbd76811762c295793391d6a909165
yesthecarlos <carlosmendez86@gmail.com>
2021-06-11 11:02:19 -0700

    add Animal and AnimalShelterContext models, update Startup.cs file, add Initial migration

---
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+  public class AnimalShelterContext : DbContext
+  {
+    public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Animal> Animals { get; set; }
+  }
+}
--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
 
 namespace AnimalShelter
 {
@@ -26,7 +28,7 @@ namespace AnimalShelter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AnimalShelterContext>(opt => opt.UseMySql(Configuration ["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -44,7 +46,7 @@ namespace AnimalShelter
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
